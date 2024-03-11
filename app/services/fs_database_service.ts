c39ebe56fb3df24db5b9fc8e3c8309f1fe3092f3
jeremy <jrm.boubee@gmail.com>
2024-03-11 13:37:50 +0100

    Add file system database service

---
+++ app/services/fs_database_service.ts
@@ -0,0 +1,68 @@
+import fs from 'node:fs/promises'
+import { dirname, join } from 'node:path'
+
+export interface DatabaseEntry {
+  title: string
+  slug: string
+  category: string
+  path: string
+}
+
+export default class FsDatabaseService {
+  constructor(private entrypoint: string) {
+    if (this.entrypoint.startsWith('.') || this.entrypoint.startsWith('/')) {
+      throw new Error(
+        'You need to provide an absolute path from the root directory of the application.',
+        {
+          cause: 'E_INVALID_DATABASE',
+        }
+      )
+    }
+  }
+  async init() {
+    const collectedSlugs = new Set()
+    const entries: DatabaseEntry[] = JSON.parse(await fs.readFile(this.entrypoint, 'utf-8'))
+
+    if (!Array.isArray(entries)) {
+      throw new Error(
+        'The json database entrypoint file must provide an array of object and start with []',
+        {
+          cause: 'E_INVALID_DATABASE',
+        }
+      )
+    }
+
+    entries.forEach((entry, index) => {
+      if (!entry.slug) {
+        throw new Error(`Invalid database entry at index ${index}. Missing "slug" !`, {
+          cause: 'E_INVALID_DATABASE',
+        })
+      }
+      if (!entry.title) {
+        throw new Error(`Invalid database entry at index ${index}. Missing "title" !`, {
+          cause: 'E_INVALID_DATABASE',
+        })
+      }
+      if (!entry.path) {
+        throw new Error(`Invalid database entry at index ${index}. Missing "path" !`, {
+          cause: 'E_INVALID_DATABASE',
+        })
+      }
+      if (!entry.category) {
+        throw new Error(`Invalid database entry at index ${index}. Missing "category" !`, {
+          cause: 'E_INVALID_DATABASE',
+        })
+      }
+      if (collectedSlugs.has(entry.slug)) {
+        throw new Error(
+          `Invalid database entry at index ${index}. Duplicated "slug", need to be unique.`,
+          { cause: 'E_INVALID_DATABASE' }
+        )
+      }
+      entry.path = join(dirname(this.entrypoint), entry.path)
+      collectedSlugs.add(entry.slug)
+    })
+    collectedSlugs.clear()
+    return entries
+  }
+}
--- start/routes.tsx
+++ start/routes.tsx
@@ -7,7 +7,16 @@
 |
 */
 
-import router from '@adonisjs/core/services/router'
+import FsDatabaseService from '#services/fs_database_service'
 import { IndexPage } from '../resources/views/pages/index.js'
+import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => <IndexPage></IndexPage>)
+router.get('/', async () => {
+  try {
+    const db = await new FsDatabaseService('database/db.json').init()
+    console.log(db)
+  } catch (error) {
+    console.log(error)
+  }
+  return <IndexPage></IndexPage>
+})
